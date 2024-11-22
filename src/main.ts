460177544a2b334b2a4f13e8209a6275a6240346
M Wahyudi HD <muh.wahyudi282828@gmail.com>
2024-11-22 12:29:23 +0700

    feat: enabling CORS to Frontend

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Mengaktifkan CORS dengan mengizinkan frontend pada port 5173
+  app.enableCors({
+    origin: 'http://localhost:5173',  
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
+    credentials: true,  // Jika diperlukan untuk mengirim cookies
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
